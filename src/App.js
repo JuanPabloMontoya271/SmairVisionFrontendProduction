df356d58299f3d303ac1f87f41dbd988ff816c31
Levi Vazquez Romo <hlvr.13@gmail.com>
2019-04-05 09:24:21 -0700

    Simple viewer starter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './scss/App.scss';
+import * as cornerstone from 'cornerstone-core';
+import * as cornerstoneWebImageLoader from 'cornerstone-web-image-loader';
+import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader';
+import * as dicomParser from 'dicom-parser';
+
+cornerstoneWebImageLoader.external.cornerstone = cornerstone;
+cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
+cornerstoneWADOImageLoader.external.dicomParser = dicomParser
+cornerstoneWADOImageLoader.configure({
+  useWebWorkers: true,
+});
+
+//Add cornerstoneWADOImageLoaderCodecs.min and cornerstoneWADOImageLoaderWebWorker.min from 
+//cornerstone-wado-image-loader to ImageLoaderScripts in public folder
+try {
+  //Configures loaders for web to avoud error: unexpected token <-
+  cornerstoneWADOImageLoader.webWorkerManager.initialize({
+      maxWebWorkers: 8,
+      startWebWorkersOnDemand: true,
+      webWorkerPath: '/ImageLoaderScripts/cornerstoneWADOImageLoaderWebWorker.min.js',
+      webWorkerTaskPaths: [],
+      taskConfiguration: {
+          decodeTask: {
+              loadCodecsOnStartup: true,
+              initializeCodecsOnStartup: false,
+              codecsPath: '/ImageLoaderScripts/cornerstoneWADOImageLoaderCodecs.min.js',
+              usePDFJS: false,
+              strict: true
+          }
+      }
+  });
+} catch (error) {
+  throw new Error('cornerstoneWADOImageLoader is not loaded');
+}
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    //Reacts dicom-img element reference to load image
+    this.dicomImg = null;
+  }
+
+  componentDidMount(){
+    //Enables HTML5 element to use cornerstone
+    cornerstone.enable(this.dicomImg)
+
+    document.getElementById('select-file').addEventListener('change',(e)=>{
+      //Extracts only one file from selected files
+      const file = e.target.files[0];
+      //Obtain specific format dicom image id 
+      const imgId = cornerstoneWADOImageLoader.wadouri.fileManager.add(file);
+      
+      this.dicomWebViewer(imgId);
+    })
+  }
+
+  dicomWebViewer = (imgId)=>{
+    //Loads image
+    cornerstone.loadImage(imgId).then((image)=>{
+      //Displays image
+      cornerstone.displayImage(this.dicomImg, image)
+    })
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section id = "dicom-web-viewer">
+        <div className = "select-file-holder">
+          <input type = "file" id = "select-file"  />
+        </div>
+        <div ref = {ref => {this.dicomImg = ref}} id="dicom-img" >
+        </div>
+      </section>
     );
   }
 }
